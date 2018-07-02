c9ea0c69f21cba3619e4fa05548debb0e87f4a4a
Dante <dante.rebellion.work@gmail.com>
2018-07-03 02:00:33 +0300

    ready for refactoring

---
+++ task-forms/src/Field.js
@@ -0,0 +1,192 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import { Col, FormGroup, Label, Input, FormFeedback, Button } from 'reactstrap';
+
+class Field extends Component {
+    constructor(props) {
+        super(props);
+        this.state = props.data;
+        console.log(props);
+    }
+
+    changeHandler = (event) => {
+        const fieldState = this.state;
+        fieldState.value = event.target.value;
+        
+        if(!fieldState.dirty) {
+            fieldState.dirty = true;
+        }
+        this.props.onChange({data: fieldState});
+        this.validate();
+    }
+
+    confirmPasswordChangeHandler = (event) => {
+        const fieldState = this.state;
+        fieldState.value = event.target.value;
+        
+        if(!fieldState.dirty) {
+            fieldState.dirty = true;
+        }
+        this.props.onChange({data: fieldState});
+        this.validateConfirmPassword();
+    }
+
+    focusHandler = () => {
+        const fieldState = this.state;
+        if(!fieldState.focused) {
+            fieldState.focused = true;
+            this.props.onFocus({data: fieldState});
+        }
+    }
+
+    confirmPasswordFocusHandler = () => {
+        const fieldState = this.state;
+        if(!fieldState.focused) {
+            fieldState.focused = true;
+            this.props.onFocus({data: fieldState});
+        }
+        this.validateConfirmPassword();
+    }
+
+    blurHandler = () => {
+        this.validate(this.state.type);
+    }
+
+    validate = () => {
+        for(let index in this.state.validators)
+            this.props.validateHandler(this.props.type, this.state.validators[index], index);
+    }
+
+    validateConfirmPassword = () => {
+        this.validate();
+        this.props.validateConfirmPasswordHandler();
+    }
+    
+    validateAll = () => {
+        for(let fieldKey in this.props.fieldsState) {
+            if(typeof this.props.fieldsState[fieldKey] === 'object') {
+                for(let index in this.props.fieldsState[fieldKey].validators) {
+                    this.props.validateHandler(fieldKey, this.props.fieldsState[fieldKey].validators[index], index);
+                }
+            } 
+        }
+    }
+
+    showErrors = (showAll) => {
+        if(showAll) {
+            const errorsList = [];
+            for(let i = 0; i < this.state.validators.length; i++) {
+                const element = <FormFeedback key={i} invalid={this.state.errors[i]}>{this.state.errors[i]}</FormFeedback>
+                errorsList.push(element);
+            }
+             return errorsList ? errorsList : false;
+        }
+    }
+
+    errorsCheck = () => {
+        let errorsCount = 0;
+        for(let i = 0; i < this.state.errors.length; i++)
+            if(this.state.errors[i])
+                errorsCount++;
+        return errorsCount ? true : false;
+    }
+
+    render() {
+        
+        const {type} = this.props;
+        
+        switch(type) {
+            case 'username':
+                return (
+                    <FormGroup row>   
+                        <Col xs={6} className="mx-auto">
+                            <Label for="username" className="label-stylish">Username</Label>
+                            <Input 
+                                type="text" 
+                                value={this.state.value}
+                                name="username" 
+                                id="username" 
+                                onChange={this.changeHandler} 
+                                onFocus={this.focusHandler}
+                                onBlur={this.validate}
+                                invalid={this.errorsCheck()}
+                            />
+                            {this.showErrors(true)}
+                        </Col>
+                    </FormGroup>
+                );
+            case 'password':
+                return (
+                    <FormGroup row>   
+                        <Col xs={6} className="mx-auto">
+                            <Label for="password" className="label-stylish">Password</Label>
+                            <Input 
+                                id="password" 
+                                type="password" 
+                                name="password"
+                                onChange={this.changeHandler}
+                                onFocus={this.focusHandler}
+                                onBlur={this.validate}
+                                invalid={this.errorsCheck()}
+                            />
+                            {this.showErrors(true)}
+                        </Col>
+                    </FormGroup>
+                );
+            case 'confirmPassword':
+                return (
+                    <FormGroup row>   
+                        <Col xs={6} className="mx-auto">
+                            <Label for="confirmPassword" className="label-stylish">Confirm password</Label>
+                            <Input 
+                                id="confirmPassword" 
+                                type="password" 
+                                name="confirmPassword"
+                                onChange={this.confirmPasswordChangeHandler}
+                                onFocus={this.confirmPasswordFocusHandler}
+                                onBlur={this.validateConfirmPassword}
+                                invalid={this.errorsCheck()}
+                            />
+                            {this.showErrors(true)}
+                        </Col>
+                    </FormGroup>
+                );    
+            case 'dateOfBirth':
+                return (
+                    <FormGroup row>   
+                        <Col xs={6} className="mx-auto">
+                            <Label for="dateOfBirth" className="label-stylish">Date of birth</Label>
+                            <Input 
+                                id="dateOfBirth" 
+                                type="text" 
+                                name="dateOfBirth" 
+                                onChange={this.changeHandler}
+                                onFocus={this.focusHandler}
+                                onBlur={this.validate}
+                                invalid={this.errorsCheck()}
+                                // placeholder="mm/dd/yyyy"
+                            />
+                            {this.showErrors(true)}
+                        </Col>
+                    </FormGroup>
+                );
+            case 'submit':
+                return(
+                    <FormGroup row>   
+                        <Col xs={1} className="mx-auto">
+                            <Button 
+                                color="success" 
+                                size="md" 
+                                className="mx-auto"
+                                onClick={this.validateAll}
+                            >
+                            Click me</Button>
+                        </Col>
+                    </FormGroup>
+                )
+        }
+    }
+}
+
+export default Field;
--- task-forms/src/index.js
+++ task-forms/src/index.js
@@ -1,8 +1,167 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Col, Form, FormGroup, Label, Input, Button, FormFeedback, FormText  } from 'reactstrap';
+import Field from './Field';
+import * as Validators from './Validators';
+//import App from './App';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class RegForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: {
+                value: '',
+                errors: [],
+                validators: [
+                    Validators.requireValidator, 
+                    Validators.usernameLengthValidator
+                ],
+                dirty: false,
+                focused: false
+            },
+            password: {
+                value: '',
+                errors: [],
+                validators: [
+                    Validators.requireValidator, 
+                    Validators.passwordLowerCaseValidator, 
+                    Validators.passwordUpperCaseValidator, 
+                    Validators.passwordNumberValidator, 
+                    Validators.passwordLengthValidator
+                ],
+                dirty: false,
+                focused: false
+            },
+            confirmPassword: {
+                value: '',
+                errors: [],
+                validators: [Validators.requireValidator, Validators.confirmPasswordValidator],
+                dirty: false,
+                focused: false
+            },
+            dateOfBirth: {
+                value: '',
+                errors: [],
+                validators: [
+                    Validators.dateFormatValidator,
+                ],
+                dirty: false,
+                focused: false
+            },
+            submit: {
+                value: '',
+                errors: [],
+                validators: [
+                    Validators.requireValidator, 
+                    Validators.usernameLengthValidator, 
+                    Validators.passwordLowerCaseValidator, 
+                    Validators.passwordUpperCaseValidator, 
+                    Validators.passwordNumberValidator, 
+                    Validators.passwordLengthValidator,
+                    Validators.dateFormatValidator
+                ],
+            },
+            mainValidator: this.validatorHandler
+        }
+    }
+    
+    validatorHandler = (name, validator, index) => {
+        const fieldState = this.state[name];
+        //console.log(fieldState.value)
+        //console.log(validator(fieldState.value))
+        fieldState.errors[index] = validator(fieldState.value);
+        this.setState({
+            [name]: fieldState
+        })
+    }
+
+    validatorConfirmPasswordHandler = () => {
+        const fieldState = this.state.confirmPassword;
+        fieldState.errors[1] = Validators.confirmPasswordValidator(this.state.password.value, this.state.confirmPassword.value);
+        this.setState({
+            confirmPassword: fieldState 
+        })
+    }
+
+    changeHandler = (data) => {
+        this.setState({
+            data
+        })
+    }
+
+    focusHandler = (data) => {
+        this.setState({
+            data
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('The form was sent');
+    }
+
+    render() {
+        return(
+            <div className="container form-margin">
+                <Form onSubmit={this.handleSubmit}>
+                    <Field 
+                        type="username"
+                        value={this.state.username.value}
+                        onFocus={this.focusHandler} 
+                        onChange={this.changeHandler}
+                        data={this.state.username}
+                        validateHandler={this.validatorHandler}
+                    />
+                    <Field 
+                        type="password" 
+                        value={this.state.password.value}
+                        // onBlur={}
+                        onFocus={this.focusHandler} 
+                        onChange={this.changeHandler}
+                        data={this.state.password}
+                        validateHandler={this.validatorHandler}
+                    />
+                    <Field 
+                        type="confirmPassword" 
+                        value={this.state.confirmPassword.value}
+                        // onBlur={}
+                        onFocus={this.focusHandler} 
+                        onChange={this.changeHandler}
+                        data={this.state.confirmPassword}
+                        passwordState={this.state.password}
+                        validateHandler={this.validatorHandler}
+                        validateConfirmPasswordHandler={this.validatorConfirmPasswordHandler}
+                    />
+                    <Field 
+                        type="dateOfBirth"
+                        value={this.state.dateOfBirth.value}
+                        // onBlur={}
+                        onFocus={this.focusHandler} 
+                        onChange={this.changeHandler}
+                        data={this.state.dateOfBirth}
+                        validateHandler={this.validatorHandler}
+                        placeholder="dd/mm/yyyy"
+                    />
+                    <Field
+                        type="submit"
+                        data={this.state.submit}
+                        fieldsState={this.state}
+                        validateHandler={this.validatorHandler}
+                    >
+                    </Field>
+                </Form>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <RegForm />,
+    document.getElementById('root')
+);
+
+
+  
