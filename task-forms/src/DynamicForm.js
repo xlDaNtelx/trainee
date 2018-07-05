d1e915a64f2a53898e7b7c9b3ef9bf973f203e69
Dante <dante.rebellion.work@gmail.com>
2018-07-05 10:41:41 +0300

    fixes with Service for form

--- task-forms/src/DynamicForm.js
+++ task-forms/src/DynamicForm.js
@@ -7,9 +7,9 @@ import * as validators from './validators';
 import { FormService } from './Service';
 
 const serviceForm = new FormService();
-let usernameValidators = [validators.requireValidator, validators.usernameLengthValidator];
-let passwordValidators = [validators.requireValidator, validators.passwordLowerCaseValidator, validators.passwordUpperCaseValidator, validators.passwordNumberValidator, validators.passwordLengthValidator];
-let dateValidators = [validators.dateFormatValidator];
+const usernameValidators = [validators.requireValidator, validators.usernameLengthValidator];
+const passwordValidators = [validators.requireValidator, validators.passwordLowerCaseValidator, validators.passwordUpperCaseValidator, validators.passwordNumberValidator, validators.passwordLengthValidator];
+const dateValidators = [validators.dateFormatValidator];
 
 class DynamicForm extends Component {
   constructor(props) {
@@ -20,79 +20,27 @@ class DynamicForm extends Component {
     serviceForm.add('dateOfBirth', {type: 'text', value: '', validators: dateValidators, className: 'some className', errors: [], dirty: false, focused: false, label: 'Date of birth'});
   }
 
-  validateForm = (fields = serviceForm.allFields) => {
-    const { password, confirmPassword } = fields;
-    if(this.isValid({password, confirmPassword})) {
-      if(password.value !== confirmPassword.value){
-        const confirmPass = {...fields.confirmPassword};
-        confirmPass.errors.push("Passoword and confirm password must be the equil");
-        fields.confirmPassword = {...confirmPass};  
-      }
-    }
-    return fields;
-  };
-
-  validateFields(fields) {
-    Object.entries(fields).forEach(([name, field]) => {
-      const { value, validators = [] } = field;
-      field.errors = validators
-        .map(validator => {
-          return validator(value);
-        })
-        .filter((error) => {
-          return error;
-        });
-    });
-    return fields;
-  }
-
-  isValid(fields) {
-    return Object.entries(fields).every(([_, field]) => {
-      return !field.errors.length;
-    });
-  }
+  
 
   handleFocus = (name) => {
-    const field = {...serviceForm.allFields[name]};
-    field.dirty = true;
-    field.focused = true;
-    serviceForm.allFields[name] = {...field};
-    serviceForm.allFields = this.validateFields(serviceForm.allFields);
-    this.validateForm(serviceForm.allFields);
+    serviceForm.onFocus(name)
     this.forceUpdate();
   };
 
   handleChange = (name, value) => {
-    const field = { ...serviceForm.allFields[name] };
-    field.value = value;
-    const fields = this.validateFields({ ...serviceForm.allFields, [name]: field });
-    serviceForm.update(name, value);
-    this.validateForm(fields);
+    serviceForm.onChange(name, value);
     this.forceUpdate();
   };
 
-  handleBlur = name => {
-    const field = {...serviceForm.allFields[name]};
-    field.focused = false;
-    serviceForm.allFields[name] = {...field };
-    serviceForm.allFields = this.validateFields(serviceForm.allFields);
-    this.validateForm(serviceForm.allFields);
+  handleBlur = (name) => {
+    serviceForm.onBlur(name);
     this.forceUpdate();
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const fields = {};
-    Object.entries(serviceForm.allFields).forEach(([name]) => {
-      const field = {...serviceForm.allFields[name]};
-      field.dirty = true;
-      fields[name] = field;
-    });
-    serviceForm.allFields = this.validateFields(fields);
+    serviceForm.onSubmit();
     this.forceUpdate();
-    if (this.isValid(serviceForm.allFields)) {
-      console.log('The form was sent');
-    }
   };
   
 
--- task-forms/src/Service.js
+++ task-forms/src/Service.js
@@ -11,8 +11,80 @@ export class FormService {
     return this.allFields;
   }
 
+  onFocus(name) {
+    const field = {...this.allFields[name]};
+    field.dirty = true;
+    field.focused = true;
+    this.allFields[name] = {...field };
+    this.allFields = this.validateFields(this.allFields);
+    this.validateForm(this.allFields);
+  }
+
+  onChange(name, value) {
+    const field = { ...this.allFields[name] };
+    field.value = value;
+    this.update(name, value);
+    const fields = this.validateFields({ ...this.allFields, [name]: field });
+    this.validateForm(fields);
+  }
+
+  onBlur(name) {
+    const field = {...this.allFields[name]};
+    field.focused = false;
+    this.allFields[name] = {...field};
+    this.allFields = this.validateFields(this.allFields);
+    this.validateForm(this.allFields);
+  }
+
+  onSubmit() {
+    const fields = {};
+    Object.entries(this.allFields).forEach(([name]) => {
+      const field = {...this.allFields[name]};
+      field.dirty = true;
+      fields[name] = field;
+    });
+    this.allFields = this.validateFields(fields);
+    this.validateForm(this.allFields);
+    if (this.isValid(this.allFields)) {
+      console.log('The form was sent');
+    }
+  }
+
   update(name, value) {
     const fields = this.getFields();
     fields[name].value = value;
   }
+
+  validateForm = (fields = this.allFields) => {
+    const { password, confirmPassword } = fields;
+    if(this.isValid({password, confirmPassword})) {
+      if(password.value !== confirmPassword.value){
+        const confirmPass = {...fields.confirmPassword};
+        confirmPass.errors.push("Passoword and confirm password must be the equil");
+        fields.confirmPassword = {...confirmPass};  
+      }
+    }
+    return fields;
+  };
+
+  validateFields(fields) {
+    Object.entries(fields).forEach(([name, field]) => {
+      const { value, validators = [] } = field;
+      field.errors = validators
+        .map(validator => {
+          return validator(value);
+        })
+        .filter((error) => {
+          return error;
+        });
+    });
+    return fields;
+  }
+
+  isValid(fields) {
+    return Object.entries(fields).every(([_, field]) => {
+      return !field.errors.length;
+    });
+  }
+
 }
